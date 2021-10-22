8ad22f5196451d11abe6c77e5480f7beacf259cf
Juan Dorado Cebrian <juan.dorado@deimos-space.com>
2021-10-22 16:02:22 +0200

    using config-server + eureka

--- catalog-services/src/main/java/com/tutorial/catalogservices/CatalogServicesApplication.java
+++ catalog-services/src/main/java/com/tutorial/catalogservices/CatalogServicesApplication.java
@@ -2,29 +2,18 @@ package com.tutorial.catalogservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.http.client.reactive.HttpComponentsClientHttpConnector;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+//@EnableHystrix
+//@EnableHystrixDashboard
 public class CatalogServicesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogServicesApplication.class, args);
-	}
-
-	@Bean
-	@LoadBalanced
-	public RestTemplate getRestTemplate() {
-		HttpComponentsClientHttpRequestFactory httpRequestFactory = new HttpComponentsClientHttpRequestFactory();
-		httpRequestFactory.setConnectionRequestTimeout(3000);
-		return new RestTemplate(httpRequestFactory);
 	}
 
 }
--- catalog-services/src/main/java/com/tutorial/catalogservices/service/client/IGameService.java
+++ catalog-services/src/main/java/com/tutorial/catalogservices/service/client/IGameService.java
@@ -14,6 +14,6 @@ public interface IGameService {
 
     @GetMapping
     @ResponseBody
-    public List<GameDto> fetch();
+    List<GameDto> fetch();
 
 }
